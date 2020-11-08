e32f6d15f8c733082636cbb4980bcd2c7c7d84e8
Alex Sukhitashvili <sukhitashvilialex2@gmail.com>
2020-11-09 00:20:31 +0400

    feat: custom table cell

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -26,6 +26,7 @@ class ChatViewController: UIViewController {
         tableView.delegate = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
 
     }
     
@@ -53,9 +54,9 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         
-        cell.textLabel?.text = messages[indexPath.row].body 
+        cell.label.text = messages[indexPath.row].body 
         
         return cell
     }
