90f2dbeff6d4818c0075c02a1d1acc3bd9660247
Alex Sukhitashvili <sukhitashvilialex2@gmail.com>
2020-11-09 02:41:47 +0400

    feat: final touches

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -15,20 +15,17 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+       
         if let email = emailTextfield.text, let password = passwordTextfield.text {
-            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let e = error {
-                    print(e.localizedDescription)
+                    print(e)
                 } else {
+                    //Navigate to the ChatViewController
                     self.performSegue(withIdentifier: K.registerSegue, sender: self)
                 }
-                
-                
             }
         }
-        
-        
-     
     }
     
 }
--- Flash Chat iOS13/Views/MessageCell.swift
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class MessageCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
-        // Initialization code
-    }
     @IBOutlet weak var messageBubble: UIView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
